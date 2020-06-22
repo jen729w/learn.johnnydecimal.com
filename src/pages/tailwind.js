899d47177b82416165b4b971fb31bca191e15814
Johnny Noble <github-a3bd@johnny.net.au>
2020-06-22 19:12:16 +1000

    More Tailwinding, more doco

--- src/pages/tailwind.js
+++ src/pages/tailwind.js
@@ -2,10 +2,37 @@ import React from "react"
 
 import "../styles/tailwind.css"
 
-const Tailwind = () => (
-  <div className="bg-red-400">
-    This is just a quick test to see if Tailwind has installed.
-  </div>
+const LearnHeader = () => (
+  <>
+    {/* J•D is active */}
+    <div className="px-4 pt-4 pb-2 font-sans font-semibold border-b-2 border-red-600 md:px-8 md:pb-1 bg-grey-100">
+      <span className="hidden font-bold text-red-600 md:inline">
+        Johnny•Decimal
+      </span>
+      <span className="font-bold text-red-600 md:hidden">J•D</span>
+      <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+      <span className="">Learn</span>
+      <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+      <span className="">Chat</span>
+      <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+      <span className="">App</span>
+    </div>
+
+    <div className="mt-10" />
+
+    {/* Learn is active */}
+    <div className="px-4 pt-4 pb-2 font-sans font-semibold border-b-2 border-orange-600 md:px-8 md:pb-1 bg-grey-100">
+      <span className="hidden md:inline">Johnny•Decimal</span>
+      <span className="md:hidden">J•D</span>
+      <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+      <span className="font-bold text-orange-600">Learn</span>
+      <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+      <span className="">Chat</span>
+      <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+      <span className="">App</span>
+    </div>
+    <div>And now the rest</div>
+  </>
 )
 
-export default Tailwind
+export default LearnHeader
