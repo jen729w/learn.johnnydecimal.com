03e7ffe45eb931e0ba5d828bab5e21b8f359d690
Johnny Noble <github-a3bd@johnny.net.au>
2020-07-06 14:39:52 +1000

    Configure custom font properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   purge: [],
   theme: {
+    fontFamily: {
+      johnnydecimal: ["Fira\\ Sans"],
+    },
     extend: {},
   },
   variants: {},
   plugins: [],
 }
+
+// Font customisation: https://tailwindcss.com/docs/font-family/#app
