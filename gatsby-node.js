f37db9064070f49f6970801f7e59f082be5d1a35
Johnny Noble <github-a3bd@johnny.net.au>
2020-07-26 12:35:41 +1000

    MDX createPages working, but ~messy
    Needs a once-over, it “works” but is a bit of a mess.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,10 @@
+const path = require("path")
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
   const blogPostTemplate = require.resolve(`./src/templates/jdSpecTemplate.js`)
+  const specMdxTemplate = require.resolve(`./src/templates/specMdxTemplate.js`)
 
   const result = await graphql(`
     {
@@ -36,4 +39,46 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       },
     })
   })
+
+  const mdxResult = await graphql(`
+    query {
+      allMdx {
+        edges {
+          node {
+            id
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  mdxResult.data.allMdx.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.slug,
+      component: specMdxTemplate,
+      context: { id: node.id, slug: node.frontmatter.slug },
+    })
+  })
 }
+
+// exports.createPages = async ({ actions, graphql, reporter }) => {
+//   const { createPage } = actions
+
+//   const result = await graphql(`
+//     query {
+//       allMdx {
+//         edges {
+//           node {
+//             id
+//             frontmatter {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
+// }
