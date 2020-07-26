d8f44b6c334c2fadab537f7bf20cbbb9aa210af7
Johnny Noble <github-a3bd@johnny.net.au>
2020-07-26 17:21:36 +1000

    OH GOD THE TEDIUM of those <pre> blocks
    Never ever do this again (again).

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,7 +1,20 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Navigation = ({ backURL, backTitle, nextURL, nextTitle }) => (
-  <nav className="flex"></nav>
+  <nav className="flex justify-between ml-4 mr-6">
+    <div className="">
+      <Link to={backURL} className="text-md">
+        ← {backTitle}
+      </Link>
+    </div>
+    <div className="text-center text-orange-700">|</div>
+    <div className="text-right">
+      <Link to={nextURL} className="text-md">
+        {nextTitle} →
+      </Link>
+    </div>
+  </nav>
 )
 
 export default Navigation
---
+++ src/spec-mdx/areas-categories/jdBlocks.js
@@ -0,0 +1,228 @@
+import React from "react"
+
+const FirstReminder = () => (
+  <pre className="inline-block px-4 py-2 text-orange-800 whitespace-pre bg-orange-100 border-2 border-orange-600 rounded-md shadow-md font-jdmono">{`23.24    31.70
+22.39    13.04
+31.71    21.02
+22.38    13.03
+23.21    23.22
+32.56    12.34
+12.35    21.01
+32.55    23.23`}</pre>
+)
+
+const TwoGroupThingsTogether = () => (
+  <pre className="inline-block px-2 py-2 text-sm text-orange-800 whitespace-pre bg-orange-100 border-2 border-orange-600 rounded-md shadow-md sm:px-4 sm:text-base font-jdmono">
+    <span className="border-b border-orange-700 border-dotted">
+      The 12 numbers
+    </span>
+    {`    `}
+    <span className="border-b border-orange-700 border-dotted">
+      The 13 numbers
+    </span>
+    <br />
+    {`    12.34  12.35      13.03  13.04`}
+    <br />
+    <br />
+    <span className="border-b border-orange-700 border-dotted">
+      The 21 numbers
+    </span>
+    {`    `}
+    <span className="border-b border-orange-700 border-dotted">
+      The 22 numbers
+    </span>
+    <br />
+    {`    21.01  21.02      22.38  22.39`}
+    <br />
+    <br />
+    <span className="border-b border-orange-700 border-dotted">
+      The 23 numbers
+    </span>
+    {`    `}
+    <span className="border-b border-orange-700 border-dotted">
+      The 31 numbers
+    </span>
+    <br />
+    {`    23.21  23.22      31.70  31.71`}
+    <br />
+    {`    23.23  23.24`}
+    <br />
+    <br />
+    <span className="border-b border-orange-700 border-dotted">
+      The 32 numbers
+    </span>
+    <br />
+    {`    32.55  32.56`}
+  </pre>
+)
+
+const ThreeDoItAgain = () => (
+  <pre className="inline-block px-4 py-2 text-sm text-orange-800 whitespace-pre bg-orange-100 border-2 border-orange-600 rounded-md shadow-md sm:text-base font-jdmono">
+    <span className="border-b-2 border-orange-600">The 10-19 numbers</span>
+    <br />
+    {`   `}
+    <span className="border-b border-orange-600 border-dotted">
+      The 12 numbers
+    </span>
+    <br />
+    {`       12.34  12.35`}
+    <br />
+    {`   `}
+    <span className="border-b border-orange-600 border-dotted">
+      The 13 numbers
+    </span>
+    <br />
+    {`       13.03  13.04`}
+    <br />
+    <br />
+    <span className="border-b-2 border-orange-600">The 20-29 numbers</span>
+    <br />
+    {`   `}
+    <span className="border-b border-orange-600 border-dotted">
+      The 21 numbers
+    </span>
+    <br />
+    {`       21.01  21.02`}
+    <br />
+    {`   `}
+    <span className="border-b border-orange-600 border-dotted">
+      The 22 numbers
+    </span>
+    <br />
+    {`       22.38  22.39`}
+    <br />
+    {`   `}
+    <span className="border-b border-orange-600 border-dotted">
+      The 23 numbers
+    </span>
+    <br />
+    {`       23.21  23.22`}
+    <br />
+    {`       23.23  23.24`}
+    <br />
+    <br />
+    <span className="border-b-2 border-orange-600">The 30-39 numbers</span>
+    <br />
+    {`   `}
+    <span className="border-b border-orange-600 border-dotted">
+      The 31 numbers
+    </span>
+    <br />
+    {`       31.70  31.71`}
+    <br />
+    {`   `}
+    <span className="border-b border-orange-600 border-dotted">
+      The 32 numbers
+    </span>
+    <br />
+    {`       32.55  32.56`}
+  </pre>
+)
+
+// const CompanyNoNumbers = () => (
+//   <>
+//     {`Folder                                 This is a...`}
+//     <br />
+//     {`———————————————————————————————————————————————————`}
+//     <br />
+//     <span className="underline">Finance</span>
+//     {`                                AREA`}
+//     <br />
+//     {`    `}
+//     <span className="border-b border-red-900">Tax returns</span>
+//     {`                        CATEGORY`}
+//     <br />
+//     {`    `}
+//     <span className="border-b border-red-900">Payroll</span>
+//     {`                            CATEGORY`}
+//     <br />
+//     {`        September ’17 payroll          UNIQUE THING`}
+//     <br />
+//     {`        October ’17 payroll            UNIQUE THING`}
+//     <br />
+//     {`    `}
+//     <span className="border-b border-red-900">Bookkeeping</span>
+//     {`                        CATEGORY`}
+//     <br />
+//     <br />
+//     <span className="underline">Administration</span>
+//     {`                         AREA`}
+//     <br />
+//     {`    `}
+//     <span className="border-b border-red-900">Company registration</span>
+//     {`               CATEGORY`}
+//     <br />
+//     {`    `}
+//     <span className="border-b border-red-900">Contracts</span>
+//     {`                          CATEGORY`}
+//     <br />
+//     {`        Cleaning contract              UNIQUE THING`}
+//     <br />
+//     {`        Office lease                   UNIQUE THING`}
+//     <br />
+//     {`    `}
+//     <span className="border-b border-red-900">Staffing</span>
+//     {`                           CATEGORY`}
+//     <br />
+//     <br />
+//     <span className="underline">Marketing</span>
+//     {`                              AREA`}
+//     <br />
+//     {`    ...etc.                            ...etc.`}
+//   </>
+// )
+
+// const CompanyWithNumbers = () => (
+//   <>
+//     {`Folder                                 This is a...`}
+//     <br />
+//     {`———————————————————————————————————————————————————`}
+//     <br />
+//     <span className="underline">10-19 Finance</span>
+//     {`                          AREA`}
+//     <br />
+//     {`   `}
+//     <span className="border-b border-red-900">11 Tax returns</span>
+//     {`                      CATEGORY`}
+//     <br />
+//     {`   `}
+//     <span className="border-b border-red-900">12 Payroll</span>
+//     {`                          CATEGORY`}
+//     <br />
+//     {`      12.01 September ’17 payroll      UNIQUE THING`}
+//     <br />
+//     {`      12.02 October ’17 payroll        UNIQUE THING`}
+//     <br />
+//     {`   `}
+//     <span className="border-b border-red-900">13 Bookkeeping</span>
+//     {`                      CATEGORY`}
+//     <br />
+//     <br />
+//     <span className="underline">20-29 Administration</span>
+//     {`                   AREA`}
+//     <br />
+//     {`   `}
+//     <span className="border-b border-red-900">21 Company registration</span>
+//     {`             CATEGORY`}
+//     <br />
+//     {`   `}
+//     <span className="border-b border-red-900">22 Contracts</span>
+//     {`                        CATEGORY`}
+//     <br />
+//     {`      22.01 Cleaning contract          UNIQUE THING`}
+//     <br />
+//     {`      22.02 Office lease               UNIQUE THING`}
+//     <br />
+//     {`   `}
+//     <span className="border-b border-red-900">23 Staffing</span>
+//     {`                         CATEGORY`}
+//     <br />
+//     <br />
+//     <span className="underline">30-39 Marketing</span>
+//     {`                        AREA`}
+//     <br />
+//     {`   ...etc.                             ...etc.`}
+//   </>
+// )
+
+export { FirstReminder, TwoGroupThingsTogether, ThreeDoItAgain }
