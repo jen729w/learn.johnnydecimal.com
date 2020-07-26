de1797f778fc538b1b5ced79c7781860be033956
Johnny Noble <github-a3bd@johnny.net.au>
2020-07-26 11:20:37 +1000

    Tidy up gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,15 +20,8 @@ module.exports = {
         path: `${__dirname}/src/md-spec`,
       },
     },
-    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    /**
-     * gatsby-remark-images: removing for now 2020-07-19 as it's not scaling
-     * images properly, but leaving as a reminder that it should probably come
-     * back at some point.
-     * https://github.com/gatsbyjs/gatsby/issues/20262)
-     */
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -43,30 +36,6 @@ module.exports = {
         ],
       },
     },
-    /**
-    * Nah second thoughts, going to just use resampled screenshots for now.
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: "gatsby-remark-copy-linked-files",
-            options: {
-              // `ignoreFileExtensions` defaults to [`png`, `jpg`, `jpeg`, `bmp`, `tiff`]
-              // as we assume you'll use gatsby-remark-images to handle
-              // images in markdown as it automatically creates responsive
-              // versions of images.
-              //
-              // If you'd like to not use gatsby-remark-images and just copy your
-              // original images to the public directory, set
-              // `ignoreFileExtensions` to an empty array.
-              ignoreFileExtensions: [],
-            },
-          },
-        ],
-      },
-    },
-    */
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
