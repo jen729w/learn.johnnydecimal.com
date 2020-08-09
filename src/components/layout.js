8a3262c97bff64ad0319faeefe3c45f5a43f7186
Johnny Noble <github-a3bd@johnny.net.au>
2020-08-09 15:17:17 +1000

    More layout tweaks

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,38 +11,43 @@ import Header from "./header"
 import TOC from "./toc"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+	const data = useStaticQuery(graphql`
+		query SiteTitleQuery {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}
+	`)
 
-  return (
-    // This div wraps the entire page
-    <div className="max-w-5xl px-4 mx-auto leading-relaxed text-offblack sm:px-8 font-jdbody">
-      <Header siteTitle={data.site.siteMetadata.title} />
-      {/* The main flexbox for nav + content */}
-      <div className="flex flex-col-reverse sm:flex-row">
-        <div className="mr-4 sm:w-1/4">
-          <TOC />
-        </div>
-        <main className="flex-grow mb-8 sm:w-3/4">{children}</main>
-      </div>
-      <footer className="bg-green-100">
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
-    </div>
-  )
+	return (
+		// This div wraps the entire page
+		<div className="max-w-5xl px-4 mx-auto leading-relaxed text-offblack sm:px-8 font-jdbody">
+			{/* TODO: Pass better metadata to <Header>, e.g. page description. */}
+			<Header siteTitle={data.site.siteMetadata.title} />
+			{/**
+			 * The main flexbox for nav + content:
+			 * - Mobile-first: flex-col-reverse, content at top, TOC below
+			 * - sm+:          flex-row, TOC on left, content to the right
+			 */}
+			<div className="flex flex-col-reverse sm:flex-row">
+				<div className="sm:w-1/4">
+					<TOC />
+				</div>
+				<main className="flex-grow mb-8 sm:w-3/4">{children}</main>
+			</div>
+			<footer className="bg-green-100">
+				© {new Date().getFullYear()}, Built with
+				{` `}
+				<a href="https://www.gatsbyjs.org">Gatsby</a>
+			</footer>
+		</div>
+	)
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 }
 
 export default Layout
--- src/components/toc.js
+++ src/components/toc.js
@@ -1,37 +1,50 @@
 import React from "react"
 import { Link } from "gatsby"
 
+const JDNavLink = ({ children, to }) => (
+	<Link to={to} activeClassName="font-semibold" className="">
+		{children}
+	</Link>
+)
+
 const TOC = () => (
-  <nav className="border border-gray-300 rounded-lg shadow">
-    <Link
-      to="/spec/home"
-      activeClassName="font-bold"
-      className="block p-1 m-1 rounded hover:bg-orange-600 hover:text-gray-100 focus:bg-orange-600 focus:text-gray-100"
-    >
-      Home
-    </Link>
-    <hr className="mb-1" />
-    <Link
-      to="/spec/concepts"
-      activeClassName="font-bold"
-      className="block p-1 m-1 rounded hover:bg-orange-600 hover:text-gray-100 focus:bg-orange-600 focus:text-gray-100"
-    >
-      Concepts
-    </Link>
-    <a href="/404" className="block mb-1">
-      Areas &amp; categories
-    </a>
-    <a href="/404" className="block mb-1">
-      IDs
-    </a>
-    <hr className="mb-1" />
-    <a href="/404" className="block mb-1">
-      Contact
-    </a>
-    <a href="/404" className="block ">
-      Privacy
-    </a>
-  </nav>
+	<nav className="border border-gray-400 rounded-md shadow-md sm:shadow-none sm:border-0">
+		{/* Mobile-only: CONTENTS label at top of box */}
+		<div className="flex justify-center sm:hidden">
+			<h1 className="px-4 -mt-4 uppercase bg-offwhite">CONTENTS</h1>
+		</div>
+		{/* Wrap all links */}
+		<div className="px-2 sm:px-0">
+			<Link
+				to="/"
+				activeClassName="font-semibold"
+				className="block p-1 m-1 rounded hover:bg-orange-600 hover:text-gray-100 focus:bg-orange-600 focus:text-gray-100"
+			>
+				Home
+			</Link>
+			<hr className="mx-2" />
+			<Link
+				to="/concepts"
+				activeClassName="font-semibold"
+				className="block p-1 m-1 rounded hover:bg-orange-600 hover:text-gray-100 focus:bg-orange-600 focus:text-gray-100"
+			>
+				Concepts
+			</Link>
+			<a href="/404" className="block mb-1">
+				Areas &amp; categories
+			</a>
+			<a href="/404" className="block mb-1">
+				IDs
+			</a>
+			<hr className="mb-1" />
+			<a href="/404" className="block mb-1">
+				Contact
+			</a>
+			<a href="/404" className="block ">
+				Privacy
+			</a>
+		</div>
+	</nav>
 )
 
 export default TOC
