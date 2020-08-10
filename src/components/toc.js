796324d6bbaab66ccd8cb60ef9c7245cc0fa96da
Johnny Noble <github-a3bd@johnny.net.au>
2020-08-10 16:49:01 +1000

    More CSS nav tweaks, this is good

--- src/components/toc.js
+++ src/components/toc.js
@@ -4,8 +4,8 @@ import { Link } from "gatsby"
 const JDNavLink = ({ children, to }) => (
 	<Link
 		to={to}
-		activeClassName="font-semibold"
-		className="block px-2 py-1 mx-2 rounded"
+		activeClassName="font-semibold sm:border-r-2 sm:border-offblack rounded-none"
+		className="block px-2 py-1 ml-2 sm:border-r-2 sm:border-gray-400"
 	>
 		{children}
 	</Link>
@@ -13,7 +13,7 @@ const JDNavLink = ({ children, to }) => (
 
 const TOC = () => (
 	<nav
-		className="border border-gray-400 rounded-md shadow-md tableofcontents sm:shadow-none sm:border-l-0 sm:border-t-0 sm:border-b-0 sm:rounded-none sm:border-r"
+		className="border border-gray-400 rounded-md shadow-md sm:mr-6 tableofcontents sm:shadow-none sm:border-none sm:rounded-none"
 		tabIndex="100"
 	>
 		{/* Mobile-only: CONTENTS label at top of box */}
@@ -23,21 +23,31 @@ const TOC = () => (
 		{/* Wrap all links */}
 		<div className="">
 			<JDNavLink to="/">Home</JDNavLink>
-			<hr className="m-2" />
-			<JDNavLink to="/concepts" activeClassName="font-semibold" className="">
+			<hr className="mx-2 my-1 border-r-2 border-gray-300" />
+			<JDNavLink to="/concepts/" activeClassName="font-semibold" className="">
 				Concepts
 			</JDNavLink>
-			<JDNavLink to="/concepts/areas-categories">
+			<JDNavLink to="/concepts/areas-categories/">
 				Areas &amp; categories
 			</JDNavLink>
-			<JDNavLink to="/categories/ids">IDs</JDNavLink>
-			<hr className="mb-1" />
-			<a href="/404" className="block mb-1">
-				Contact
-			</a>
-			<a href="/404" className="block ">
-				Privacy
-			</a>
+			<JDNavLink to="/concepts/ids/">IDs</JDNavLink>
+			<JDNavLink to="/concepts/saving-files/">Saving files</JDNavLink>
+			<JDNavLink to="/concepts/keeping-notes/">Keeping notes</JDNavLink>
+			<JDNavLink to="/concepts/tracking-your-numbers/">
+				Tracking your numbers
+			</JDNavLink>
+			<JDNavLink to="/concepts/developing-your-own-system">
+				Developing your own system
+			</JDNavLink>
+			<hr className="mx-2 my-1 border-r-2 border-gray-300" />
+			<JDNavLink to="/concepts/managing-email/">Managing email</JDNavLink>
+			<JDNavLink to="/concepts/what-about-00-09/">What about 00-09?</JDNavLink>
+			<JDNavLink to="/concepts/working-at-the-terminal/">
+				Working at the terminal
+			</JDNavLink>
+			<JDNavLink to="/concepts/exceptions-to-the-rules/">
+				Exceptions to the rules
+			</JDNavLink>
 		</div>
 	</nav>
 )
