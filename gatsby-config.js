9db723582928814ce8c26177d67345c22f70fe98
Johnny Noble <github-a3bd@johnny.net.au>
2021-01-05 14:20:25 +1100

    🎆 2021! small tidy-up

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,20 +44,6 @@ module.exports = {
 				],
 			},
 		},
-		// {
-		// 	resolve: `gatsby-transformer-remark`,
-		// 	options: {
-		// 		plugins: [
-		// 			{
-		// 				resolve: "gatsby-remark-smartypants",
-		// 				options: {
-		// 					backticks: false,
-		// 					dashes: "oldschool",
-		// 				},
-		// 			},
-		// 		],
-		// 	},
-		// },
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
