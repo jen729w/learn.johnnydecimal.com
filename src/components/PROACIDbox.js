accd5843c705a169820e9873473c121f219450ee
Johnny Noble <github-a3bd@johnny.net.au>
2021-01-08 13:35:20 +1100

    CONSTs being loaded on to components working
    For <Project>, need to expand out to <Area> & <ID>

--- src/components/PROACIDbox.js
+++ src/components/PROACIDbox.js
@@ -1,23 +1,45 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-export const PROACIDbox = ({ children, classes }) => (
-	<div className="text-base bg-blue-900 rounded-md font-jdmono text-offwhite">
-		<div className="py-1 pr-2 text-sm text-right text-white bg-blue-600 rounded-t-md">
-			J•D
-		</div>
-		<div className="p-2">{children}</div>
-	</div>
-)
+export const PROACIDbox = ({ children, classes }) => {
+	const PROJECT_MARGINLEFT = "4ch"
+	const PROJECT_TEXTINDENT = "-4ch"
 
-export const Project = ({ children, classes }) => (
-	<div
-		className={`font-bold underline ${classes}`}
-		style={{ marginLeft: "4ch", textIndent: "-4ch" }}
-	>
-		{children}
-	</div>
-)
+	const childrenWithProps = React.Children.map(children, child =>
+		React.cloneElement(child, {
+			project_marginLeft: PROJECT_MARGINLEFT,
+			project_textIndent: PROJECT_TEXTINDENT,
+		})
+	)
+
+	return (
+		<div className="text-base bg-blue-900 rounded-md font-jdmono text-offwhite">
+			<div className="py-1 pr-2 text-sm text-right text-white bg-blue-600 rounded-t-md">
+				J•D
+			</div>
+			<div className="p-2">{childrenWithProps}</div>
+		</div>
+	)
+}
+
+export const Project = ({ children, classes, ...rest }) => {
+	const { project_marginLeft, project_textIndent } = rest
+	if (
+		typeof project_marginLeft === "undefined" ||
+		typeof project_textIndent === "undefined"
+	) {
+		throw new Error("The Project component was not passed the relevant props.")
+	}
+
+	return (
+		<div
+			className={`font-bold underline ${classes}`}
+			style={{ marginLeft: project_marginLeft, textIndent: project_textIndent }}
+		>
+			{children}
+		</div>
+	)
+}
 
 export const Area = ({ children, classes }) => (
 	<div
