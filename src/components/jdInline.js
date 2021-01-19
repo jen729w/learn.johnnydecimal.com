2a72c7bfe8f8d87b4309d6efd0dee7a65572a3a6
Johnny Noble <github-a3bd@johnny.net.au>
2021-01-20 07:45:25 +1100

    🕺🏾 `this` is now <JD> in .blog-post! Of course!
    Also you made <JD yellow> but you’ve already superseded it. :-)

--- src/components/jdInline.js
+++ src/components/jdInline.js
@@ -1,5 +1,18 @@
+/* eslint-disable react/prop-types */
 import React from "react"
 
-const JD = ({ children }) => <span className="jd-inline">{children}</span>
+const JD = ({ children, yellow = false }) => {
+	let colour = "blue"
+	if (yellow) {
+		colour = "yellow"
+	}
+	return (
+		<span
+			className={`px-1 whitespace-pre break-words border border-${colour}-700 rounded-sm bg-${colour}-50 font-jdmono`}
+		>
+			{children}
+		</span>
+	)
+}
 
 export default JD
